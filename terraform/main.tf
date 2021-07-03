9d2ed346fd7933de35a9909153f0f95fecf125de
Evans Thiringai Njoroge <68475646+thiringai-evans@users.noreply.github.com>
2021-07-04 01:09:34 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,6 +54,39 @@ resource "aws_eip" "flask-access" {
   }
 }
 
+resource "aws_s3_bucket" "thiringai-terraform" {
+  bucket = "thiringai-terraform"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  tags ={
+    Name = "S3 Remote Terraform State Store"
+  }
+}
+
+# create a dynamodb table for locking the state file
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "terraform-state-lock-dynamo"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "DynamoDB Terraform State Lock Table"
+  }
+}
+
 output "ec2_public_ip" {
   value = aws_instance.TF-instance.public_ip
 }
